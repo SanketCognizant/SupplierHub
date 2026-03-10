6abaffcc4d05445916ea36f4368f7824fb09cff4
Nayak <2478812@cognizant.com>
2026-03-10 09:50:28 +0530

    created ApprovalRule Model

---
+++ SupplierHub/Models/ApprovalRuleModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupplierHub.Models
+{
+	public class ApprovalRuleModel
+	{
+		[Key]
+		public int? RuleID { get; set; }
+		[Column(TypeName ="nvarchar(20)")]
+		public string? Scope { get; set; }
+		[Column(TypeName ="nvarchar(max)")]
+		public string? ExpressionJSON { get; set; }
+		[Column(TypeName ="nvarchar(20")]
+		public string? Severity { get; set; }
+
+		public bool? Status { get; set; }
+	}
+}
